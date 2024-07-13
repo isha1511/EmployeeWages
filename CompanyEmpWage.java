9a6c9d18d13137c183433569f04fb1d7895316b1
isha_15 <makhijaisha1511@gmail.com>
2024-07-13 21:53:26 +0530

    Added:companywiseEmpWage

--- CompanyEmpWage.java
+++ CompanyEmpWage.java
@@ -2,10 +2,27 @@ public class CompanyEmpWage {
     public String companyName;
     public int wagePerHr;
     public int numWorkingDays;
-   public int maxHrsPerMonth;
+    public int maxHrsPerMonth;
     public int totalEmpWage;
-    public CompanyEmpWage(String companyName, int wagePerHr) {
+    public CompanyEmpWage(String companyName, int wagePerHr, int numWorkingDays, int maxHrsPerMonth) {
         this.companyName = companyName;
         this.wagePerHr = wagePerHr;
+        this.numWorkingDays = numWorkingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
+
+    public int getTotalEmpWage() {
+        return totalEmpWage;
+    }
+
+    public void setTotalEmpWage(int totalEmpWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyEmpWage" +
+                "Company Name :"+companyName+
+                "totalEmpWage :" + totalEmpWage;
     }
 }
