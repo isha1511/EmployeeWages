b8331f9ccf366034d4f190460d753406b6667f68
isha_15 <makhijaisha1511@gmail.com>
2024-07-13 19:12:59 +0530

    Added:PartTimeAndSwitch

---
+++ CompanyEmpWage.java
@@ -0,0 +1,11 @@
+public class CompanyEmpWage {
+    public String companyName;
+    public int wagePerHr;
+    public int numWorkingDays;
+   public int maxHrsPerMonth;
+    public int totalEmpWage;
+    public CompanyEmpWage(String companyName, int wagePerHr) {
+        this.companyName = companyName;
+        this.wagePerHr = wagePerHr;
+    }
+}
